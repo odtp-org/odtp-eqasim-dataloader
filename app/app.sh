3e20f83bcfbc7d303fe099fc2699d5000b734e5d
caviri <carlosvivarrios@gmail.com>
2024-03-08 17:06:57 +0100

    Updating dataloader to pass input to output

--- app/app.sh
+++ app/app.sh
@@ -5,5 +5,9 @@ wget $LINK/download
 mv download download.zip
 unzip download.zip -d /odtp/odtp-output
 
+if [ "$TRANSFER_INPUT_TO_OUTPUT" == "TRUE" ]; then
+    cp -r /odtp/odtp-input/* /odtp/odtp-output
+fi
+
 # All the output folder and files are placed in odtp-output
 cp -r /odtp/odtp-workdir/output/* /odtp/odtp-output
