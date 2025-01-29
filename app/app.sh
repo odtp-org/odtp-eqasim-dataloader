3c95d8e1e9cb42a5e18d004e05a1cd5ae7cbe8f0
Carlos Vivar Rios <45425937+caviri@users.noreply.github.com>
2025-01-29 10:12:00 +0100

    feat: Improved logic and log of downloaded

--- app/app.sh
+++ app/app.sh
@@ -3,12 +3,9 @@
 # Downloading Link provided
 # The option --progress=bar:force:noscroll makes the log output less verbose
 # While the progressbar is displayed only one line is added for it in the logs
-wget --progress=bar:force:noscroll -O download.zip  $LINK/download
-echo "Downloaded file with user id and password. Now procedding with unzip"
-unzip -q download.zip -d /odtp/odtp-output
-echo "Uncompressed file."
-
 if [ -n "$PASSWORD" ]; then
+    echo "PASSWORD variable found. Downloading link"
+    echo $LINK
     # Check if the required environment variables are set
     if [ -z "$LINK" ] || [ -z "$PASSWORD" ]; then
         echo "Error: LINK and PASSWORD environment variables must be set."
@@ -24,5 +21,12 @@ if [ -n "$PASSWORD" ]; then
     echo "Downloaded file with user id and password. Now procedding with unzip"
     unzip -qo download.zip -d /odtp/odtp-output
     
+    echo "Uncompressed file."
+else
+    echo "PASSWORD variable not found. Downloading link:"
+    echo $LINK
+    wget --progress=bar:force:noscroll -O download.zip  $LINK/download
+    echo "Downloaded file with user id and password. Now procedding with unzip"
+    unzip -q download.zip -d /odtp/odtp-output
     echo "Uncompressed file."
 fi
